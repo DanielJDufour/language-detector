81fa72f900c1785a6988b0bc1c9b636f2c3340d1
DanielJDufour <daniel.j.dufour@gmail.com>
2019-07-10 13:55:43 -0400

    updates

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-  name = 'language-detector',
-  packages = ['language_detector'],
-  package_dir = {'language_detector': 'language_detector'},
-  package_data = {'language_detector': ['prep/char_language.txt','tests/__init__.py','tests/test.py']},
-  version = '4.3',
-  description = 'Detect language of text',
-  author = 'Daniel J. Dufour',
-  author_email = 'daniel.j.dufour@gmail.com',
-  url = 'https://github.com/DanielJDufour/language-detector',
-  download_url = 'https://github.com/DanielJDufour/language-detector/tarball/download',
-  keywords = ['arabic','farsi','french','kurdish','kurmanci','language','python','sorani','spanish','tagging','turkish'],
-  classifiers = [],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'language-detector',
+    packages = ['language_detector'],
+    package_dir = {'language_detector': 'language_detector'},
+    package_data = {'language_detector': ['prep/char_language.txt','tests/__init__.py','tests/test.py']},
+    version = '5.0.0',
+    description = 'Detect language of text',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = 'Daniel J. Dufour',
+    author_email = 'daniel.j.dufour@gmail.com',
+    url = 'https://github.com/DanielJDufour/language-detector',
+    download_url = 'https://github.com/DanielJDufour/language-detector/tarball/download',
+    keywords = ['arabic','farsi','french','kurdish','kurmanci','language','python','sorani','spanish','tagging','turkish'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
 )
